f0a767f7eabc61d9493715b9219413345dd9e90d
Mehran Meidani <m.meidani@me.com>
2016-08-16 11:26:36 +0430

    Add unicode function to models

--- Remembering/models.py
+++ Remembering/models.py
@@ -21,10 +21,15 @@ class Person(models.Model):
     url = models.SlugField(unique=True, default=uuid.uuid1)
     is_url_valid = models.BooleanField(default=True)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Message(models.Model):
     person = models.ForeignKey(Person)
     text_message = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to=get_file_path, blank=True, null=True)
 
-
+    def __unicode__(self):
+        return "A greeting message from '%s' which is '%s'" % (self.person.name,
+                                                               'text' if self.text_message is not None else 'image')
